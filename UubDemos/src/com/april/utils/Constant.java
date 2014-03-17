236e1e8c15ff987cad95ff0fa55d068e8dc0ef0e
April_0409 <null1989@qq.com>
2014-03-17 19:03:23 +0800

    实现从系统图库选择图片，并显示，且存储至自定义的文件夹中
    存储至自定义的文件夹中，这部分操作可以考虑用子线程或是service来实现

--- UubDemos/src/com/april/utils/Constant.java
+++ UubDemos/src/com/april/utils/Constant.java
@@ -4,4 +4,7 @@ public class Constant {
 
     public static final int RESULT_CAMERA = 0;
     public static final int RESULT_GALLERY = 1;
+
+    public static final String URI_IMAGE = "/1uub/diary";
+    public static final String IMAGE_PREFIX = "uub_";
 }
--- UubDemos/src/com/april/uubdemos/MainActivity.java
+++ UubDemos/src/com/april/uubdemos/MainActivity.java
@@ -1,12 +1,20 @@
 package com.april.uubdemos;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Media;
 import android.view.Menu;
@@ -18,6 +26,7 @@ import com.april.utils.Utils;
 public class MainActivity extends Activity {
 
     private ImageView mImageView;
+    private File mStorePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,43 @@ public class MainActivity extends Activity {
 
         mImageView = (ImageView) this.findViewById(R.id.imageView1);
 
-        callCamera();
-        //        callGallery();
+        mStorePath = initStorePath(this);
+
+        //        callCamera();
+        callGallery();
+    }
+
+    private File initStorePath(Context context) {
+        StringBuilder path = null;
+        File file = null;
+        //优先选择外部存储卡
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            //正常
+            File external = Environment.getExternalStorageDirectory();
+            path = new StringBuilder(external.getPath());
+
+        } else {
+            // 内部存储
+            File internal = Environment.getDataDirectory();
+            // '/storage/sdcard0 ' 
+            path = new StringBuilder(internal.getPath());
+        }
+
+        if (null != path) {
+            path.append(Constant.URI_IMAGE);
+            //若path不存在，则需创建
+            file = new File(path.toString());
+            if (!file.exists()) {
+                boolean is = file.mkdirs();
+                Utils.log("not exists");
+                Utils.log("success :" + is);
+            } else {
+                Utils.log("exists");
+            }
+
+        }
+
+        return file;
     }
 
     @Override
@@ -49,6 +93,8 @@ public class MainActivity extends Activity {
     private void callCamera() {
         Intent intent = new Intent();
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.parse(mStorePath.getAbsolutePath()));
+        Utils.log("store path: " + Uri.parse(mStorePath.getAbsolutePath() + "/1.jpg"));
         startActivityForResult(intent, Constant.RESULT_CAMERA);
     }
 
@@ -69,7 +115,7 @@ public class MainActivity extends Activity {
             Utils.log("resultCode = " + resultCode);
             if (Activity.RESULT_OK == resultCode && null != data) {
                 Utils.log("yes");
-
+                // 发intent时，没有指定MediaStore.EXTRA_OUTPUT时，才会返回data
                 // 获取返回的bitmap
                 Bundle bundle = data.getExtras();
                 Bitmap bitmap = (Bitmap) bundle.get("data");
@@ -80,6 +126,7 @@ public class MainActivity extends Activity {
 
             } else {
                 Utils.log("no");
+
             }
             break;
 
@@ -94,14 +141,55 @@ public class MainActivity extends Activity {
                 String imagePath = c.getString(columnIndex);
                 c.close();
 
+                //  03-17 17:56:42.304: D/UubDemos(5605): imagePath : /storage/sdcard0/DCIM/Camera/20140317_174236.jpg
                 Utils.log("imagePath : " + imagePath);
 
-                // TODO 这里需要考虑是不是需要另存一份图片们到某个路径下，相应的数据库里就存该图片的uri
+                // 显示被选中的图片
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+                BitmapDrawable bitmapDrawable = new BitmapDrawable(bitmap);
+                mImageView.setBackgroundDrawable(bitmapDrawable);
 
+                // TODO 这里需要考虑是不是需要另存一份图片们到某个路径下，相应的数据库里就存该图片的uri
+                // 考虑在service里面做呀
+                File imageFile = new File(imagePath);
+                String imageName = imageFile.getName();
+                StringBuilder desSb = new StringBuilder(mStorePath.getAbsolutePath()).append("/")
+                        .append(Constant.IMAGE_PREFIX).append(imageName);
+                try {
+                    testCopyPic(imagePath, desSb.toString());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
             break;
         }
 
     }
 
+    private void testCopyPic(String src, String des) throws IOException {
+        int byteRead = 0;
+
+        File srcFile = new File(src);
+        File desFile = new File(des);
+
+        if (srcFile.exists()) {
+            FileInputStream inStream = new FileInputStream(srcFile);
+            FileOutputStream outStream = new FileOutputStream(desFile);
+
+            byte[] buffer = new byte[(int) srcFile.length()];
+            Utils.log("src size : " + srcFile.length());
+
+            while ((byteRead = inStream.read(buffer)) != -1) {
+                outStream.write(buffer, 0, byteRead);
+
+            }
+            inStream.close();
+            outStream.close();
+
+        } else {
+            return;
+        }
+
+    }
 }
